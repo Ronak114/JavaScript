a5dad94d98ba888c17aec7e0831a44d877346131
Ronak Pawar <97387836+Ronak114@users.noreply.github.com>
2024-02-16 04:35:32 +0000

    Datatypes

---
+++ 01_basic/01_varConst.js
@@ -0,0 +1,24 @@
+// Variables and Constant
+
+//To declare constant
+const regNo = 2021022
+
+//To declare variable - let and var(mostly not used..bcz of scope)
+let email = "abc@gmail.com"
+var emailPass = "12345"
+
+//Another way ..not preferable
+city = "Ahmedabad"
+
+//initialization
+let accState;
+
+// regNo=20000 (chnage is not allowed)
+console.log(regNo);
+
+email = "xyz@gmail.com"
+emailPass = "098765"
+city = "Bengaluru"
+
+//To print in table form
+console.table([email, emailPass, city,accState])
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,28 @@
+// Data Types
+
+"use strict"; // treat all JS code as new version
+// alert(3+3) // we are using nodejs,not browser
+
+//No readability 
+console.log(1 + 0); console.log("Ronak");
+
+//documentation-->mdn(we used) but original tc39(ecma)
+
+// *******Primitve datatype*******
+// string =>""
+// number => 2^53
+// bigint
+// boolean = "true"
+// null => standalone value (empty)
+// undefined
+// symbol =>unique
+
+let name = "Ronak"
+let age = 21
+let isLoggedIn = true
+
+//Object
+console.log(typeof"Ronak");
+console.log(typeof age);
+console.log(typeof null); // op -->null
+console.log(typeof undefined); // op-->undefined
