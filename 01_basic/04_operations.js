61aa838ad3dbde56b2ee4d52cc4f393e0ba544df
Ronak Pawar <97387836+Ronak114@users.noreply.github.com>
2024-02-20 04:37:19 +0000

    comparison

---
+++ 01_basic/04_operations.js
@@ -0,0 +1,32 @@
+//*** Operation *******
+let val = 5;
+let negVal = -val;
+console.log(negVal)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**3);
+console.log(2%3);
+
+let str1="hello"
+let str2=" Ronak"
+console.log(str1 + str2);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + "2");
+console.log(1 + 2);
+console.log(1 + 2 + "2"); //OP - 32
+console.log("1" + 2 + 2); //OP - 122
+// ********** ECMA **********
+
+//NOT used in coperate
+console.log(+true);
+console.log(-true);
+console.log(+"");
+console.log(-"");
+// go for readability
+
+//Prefix and postfix..mdn js documentation
---
+++ 01_basic/05_comparison.js
@@ -0,0 +1,8 @@
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log();
+//==(equality check) and > < (comparison) works differently
+
+//strict change ===
+//check val and datatype
