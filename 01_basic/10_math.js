f148768645cd9b78c8ca5fc2e4318e0e9fb5ec9d
Ronak Pawar <97387836+Ronak114@users.noreply.github.com>
2024-02-24 06:24:19 +0000

    Math Function

---
+++ 01_basic/10_math.js
@@ -0,0 +1,20 @@
+/* ****************** MATHS *******************/
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.3));
+console.log(Math.round(4.6));
+console.log(Math.floor(4.6));
+console.log(Math.min(4, 1, 8, 6));
+console.log(Math.max(4, 1, 8, 6));
+
+//Random
+console.log(Math.random());//alys betn 0 and 1
+console.log(Math.floor(Math.random() * 10) + 1); // to be always greater than 0.
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
+// In console -> prototype other methods.
