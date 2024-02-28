b8f6234b52e049fb78d4f42f2b7316059c2a5257
Ronak Pawar <97387836+Ronak114@users.noreply.github.com>
2024-02-28 06:03:16 +0000

    while and do while loop

---
+++ 04_iteration/02_loop.js
@@ -0,0 +1,20 @@
+// // while loop
+// let i = 0
+// while (i <= 10) {
+//     console.log(i);
+//     i++
+// }
+
+// let myArr = ["a", "b", "c", "d"]
+// let j = 0;
+// while (j < myArr.length) {
+//     console.log(myArr[j]);
+//     j++
+// }
+
+//do while loop
+let val = 11
+do {
+    console.log(val);
+    val++
+} while (val<=5);
