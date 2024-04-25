3b051535621332320a32101aad389f337c2d2f43
Ronak Pawar <97387836+Ronak114@users.noreply.github.com>
2024-04-25 18:23:26 +0530

    Fetch

---
+++ 08_API/a3_fetch.js
@@ -0,0 +1,9 @@
+// FETCHING DATA USING FETCH.
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
